27a780bc0f283d45bf6713d9508fa0217655c342
Canche (U) <alexander.lopez@ufm.edu>
2026-02-14 13:31:08 -0600

    Header finish
    Se modifico el componente de header y practicamente esta terminado

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../../components/header/header';
+import { ColumnaDerecha } from '../../components/columna-derecha/columna-derecha'
+import { ColumnaIzquierda } from '../../components/columna-izquierda/columna-izquierda'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ColumnaDerecha, ColumnaIzquierda],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
