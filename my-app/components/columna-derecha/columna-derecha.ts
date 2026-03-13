e49f0c7862062cf35b29910ef291ddd33a77edf1
Canche (U) <alexander.lopez@ufm.edu>
2026-03-13 15:31:41 -0600

    Public API (PokeApi)
    Muestra info acerca del pokemon Charizard

--- my-app/components/columna-derecha/columna-derecha.ts
+++ my-app/components/columna-derecha/columna-derecha.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf, TitleCasePipe } from '@angular/common';
+import { PokeService } from '../../src/app/components/service_data/poke';
 import { GithubService } from '../../src/app/components/service_data/github';
 
 @Component({
   selector: 'app-columna-derecha',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, TitleCasePipe],
   templateUrl: './columna-derecha.html',
   styleUrls: ['./columna-derecha.css']
 })
 export class ColumnaDerecha implements OnInit {
 
   repos: any[] = [];
+  charizard: any = null
 
-  constructor(private githubService: GithubService) {}
+  constructor(
+    private githubService: GithubService,
+    private pokeService: PokeService
+  ) {}
 
   ngOnInit() {
     this.githubService.getRepos().subscribe(data => {
       this.repos = data.slice(0, 3);
     });
+
+  this.pokeService.getCharizard().subscribe(data => {
+    this.charizard = data;
+  });
   }
 
 }
